733a2263e6f4ad94a9f29053a401cd3c149e8d7a
jsotteccanic <jsotteccanic@gmail.com>
2018-06-09 21:52:43 -0500

    Avance 2

--- js/logica.js
+++ js/logica.js
@@ -1,7 +1,29 @@
 'use strict';
 var btn = document.getElementById('btnEntregado');
 var nroEntrega = document.getElementById('nroEntrega');
+var camara = document.getElementById('cameraInput');
+var preview = document.getElementById('imgShow');
 
+camara.addEventListener('change',()=>{
+
+    var file    = document.getElementById('cameraInput').files[0];
+    var reader  = new FileReader();
+
+    reader.onloadend = function () {
+      preview.src = reader.result;
+    }
+  
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      preview.src = "";
+    }
+});
 btn.addEventListener("click",()=>{
-    alert("se entregó satisfactoriamente el paquete nro :" + nroEntrega.value);
+    if(preview == ""){
+        alert("Falta verificar");
+    }else{
+        alert("se entregó satisfactoriamente el paquete nro :" + nroEntrega.value);
+    }
+    
 })
